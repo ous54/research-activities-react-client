72bb0e9d2f404d06418d06ec2d16165a785ef764
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-02-27 09:31:28 +0100

    Update AuthorHeader.js

--- src/components/author/AuthorHeader.js
+++ src/components/author/AuthorHeader.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const AuthorHeader = props => {
+  const handleFollow = () => {
+    props.suivre();
+  };
+
   return (
     <div className="card">
       <div className="card-body">
@@ -9,13 +13,14 @@ const AuthorHeader = props => {
             className="avatar avatar-xxl mr-5 "
             style={{
               backgroundImage: props.url_picture
-            }} 
+            }}
           ></span>
           <div className="media-body">
             <h4 className="m-0">
               {props.name}
               <button
                 type="button"
+                onClick={handleFollow}
                 className="btn btn-outline-primary btn-sm m-2"
               >
                 <i className="fe fe-plus mr-2"></i>Suivre
@@ -29,7 +34,7 @@ const AuthorHeader = props => {
             <div className=" list-inline mb-0 mt-2">
               {props.interests.map(interest => (
                 <a
-                  href="#"
+                  href
                   key={interest}
                   className="btn btn-primary btn-sm mb-2 mr-2 mb-1"
                 >
