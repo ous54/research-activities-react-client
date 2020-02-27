61c27c89f14b1f99d3054a387c94ed93351788ea
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-02-27 09:38:33 +0100

    Update Publication.js

--- src/components/author/Publication.js
+++ src/components/author/Publication.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React,{ useState } from "react";
 
 import { useHttp } from "../../hooks/http";
 
 const Publication = props => {
+  
   const ELSEVIER_API_URL = process.env.REACT_APP_ELSEVIER_API_URL;
   const ELSEVIER_API_KEY = process.env.REACT_APP_ELSEVIER_API_KEY;
   const TITLE = props.publication.bib.title;
+
+  const [loading, setloading] = useState(false)
 
   const url =
     ELSEVIER_API_URL +
@@ -19,19 +22,13 @@ const Publication = props => {
     props.publication
   ]);
 
-  if (!isDetailsLoading && !PublicationDetails) {
-    console.log("not working for :" + TITLE);
-  }
 
   let publication_SJR = <div className="loader container "></div>;
   let publication_IF = <div className="loader container "></div>;
 
   if (PublicationDetails) {
     if (PublicationDetails["serial-metadata-response"].error) {
-      console.log(PublicationDetails["serial-metadata-response"].error);
-      publication_SJR = (
-        <div>{PublicationDetails["serial-metadata-response"].error}</div>
-      );
+      publication_SJR = <span className="badge badge-default">not found</span>;
     } else {
       publication_SJR =
         PublicationDetails["serial-metadata-response"]["entry"][0].SJRList
