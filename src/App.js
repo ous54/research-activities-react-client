aa9d8ad468ce231b8e05e2a0757803a4bb548f4e
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-02-27 09:32:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,84 +1,39 @@
 import React from "react";
 
-import {
-  withRouter,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { withRouter, Switch, Route } from "react-router-dom";
 
 import "./assets/css/dashboard.css";
 
 import Author from "./pages/Author";
 import Home from "./pages/Home";
+import Universities from "./pages/Universities";
+import Schools from "./pages/Schools";
+import Laboratories from "./pages/Laboratories";
+import Users from "./pages/Users";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import NavBar from "./components/layout/NavBar";
 
 const App = withRouter(({ history }) => {
-  const handleKeyDown = e => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-
-      const authorName = e.target.value;
-
-      if (authorName.trim().length === 0) return;
-
-      history.push("/author/" + authorName);
-    }
-  };
-
   return (
     <div className="page">
       <div className="flex-fill">
         <Header />
-        <div className="header collapse d-lg-flex p-0" id="headerMenuCollapse">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-lg-3 ml-auto">
-                <form className="input-icon my-3 my-lg-0">
-                  <input
-                    type="search"
-                    onKeyDown={handleKeyDown}
-                    className="form-control header-search"
-                    placeholder="Search for an author"
-                  />
-                  <div className="input-icon-addon">
-                    <i className="fe fe-search"></i>
-                  </div>
-                </form>
-              </div>
-              <div className="col-lg order-lg-first">
-                <ul className="nav nav-tabs border-0 flex-column flex-lg-row">
-                  <li className="nav-item">
-                    <a
-                      className="nav-link"
-                      onClick={e => {
-                        history.push("/home");
-                      }}
-                    >
-                      <i className="fe fe-home"></i> Home
-                    </a>
-                  </li>
-
-                  <li className="nav-item">
-                    <a
-                      onClick={e => {
-                        history.push("/author/aa");
-                      }}
-                      className="nav-link"
-                    >
-                      <i className="fe fe-user"></i> Author
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <NavBar />
         <div className="my-3 my-md-5">
           <div className="container">
             <Switch>
               <Route path="/home" children={<Home />} />
-              <Route path="/author/:authorName" children={<Author />} />
+              <Route path="/users" children={<Users />} />
+              <Route path="/author/" children={<Author />} />
+              <Route path="/universities/" children={<Universities />} />
+              <Route path="/schools/" children={<Schools />} />
+              <Route path="/laboratories/" children={<Laboratories />} />
+              <Route path="/Login/" children={<Login />} />
+              <Route path="/Register/" children={<Register />} />
             </Switch>
           </div>
         </div>
