8c9d573daf96d4e1b3e5064c92fee70a17f958de
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-09 04:11:32 +0100

    Create UserAvatar.js

---
+++ src/components/Managing/UserAvatar.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useState } from "react";
+
+function UserAvatar(props) {
+  const { user } = props;
+  return (
+    <Fragment>
+      <div class="row row-sm align-items-center">
+        <div class="col-auto">
+          <span class="avatar avatar-md">
+            {user.firstName ? user.firstName[0] : ""}
+            {user.lastName ? user.lastName[0] : ""}
+          </span>
+        </div>
+        <div class="col">
+          <h3 class="mb-0">
+            <a href="#">
+              {user.firstName ? user.firstName : ""}{" "}
+              {user.lastName ? user.lastName : ""}
+            </a>
+          </h3>
+          <div class="text-muted text-h5">{user.email ? user.email : ""}</div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default UserAvatar;
