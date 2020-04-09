41bf7bdfc61402f5d895ff5fec51ca7ca62cf2ea
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-09 04:03:14 +0100

    Update App.js
    Updating React Route

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,28 @@
-import React  from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { Router, Route as PublicRoute, Switch } from "react-router-dom";
 
-import { createBrowserHistory } from 'history';
+import MainLayout from "../pages/MainLayout";
+import LoginPage from "../pages/Auth/LoginPage";
+import RegisterPage from "../pages/Auth/RegisterPage";
+import Route from "../components/Route";
 
+import { history } from "../helpers/history";
 
-import  PrivateRoute  from "../components/PrivateRoute";
-import  MainLayout  from "../pages/MainLayout";
-import  LoginPage  from "../pages/Auth/LoginPage";
-import  RegisterPage from "../pages/Auth/RegisterPage";
+import "../assets/css/datatables.css";
+import "../assets/css/tabler.css";
 
-import {history} from "../helpers/history"
-
-import "../assets/css/datatables.css"
-import "../assets/css/tabler.css"
-import "../assets/css/tabler-flags.css"
-
-import 'popper.js/dist/popper'
-import "bootstrap/dist/js/bootstrap"
-
+import "popper.js/dist/popper";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
-
   return (
-      <Router history={history}>
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <PrivateRoute exact path="/*" component={MainLayout} />
-        </Switch>
-      </Router>
+    <Router history={history}>
+      <Switch>
+        <PublicRoute path="/login" component={LoginPage} />
+        <PublicRoute path="/register" component={RegisterPage} />
+        <Route path="/*" component={MainLayout} />
+      </Switch>
+    </Router>
   );
 }
 
