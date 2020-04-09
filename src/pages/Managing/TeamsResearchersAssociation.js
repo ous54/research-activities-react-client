bcb43882ba2bf859cd14b566ce0b961aa9a46c75
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-09 04:08:48 +0100

    Create TeamsResearchersAssociation.js

---
+++ src/pages/Managing/TeamsResearchersAssociation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamsResearchersAssociation = (props) => {
+  return (
+    <div class="container">
+      <div class="page-header">
+        <div class="row align-items-center">
+          <div class="col-auto">
+            <h2 class="page-title">Teams Researchers Association</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamsResearchersAssociation;
