4c780615fbb6ff24fd08ca05dd86e0014ceaa19b
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-09 04:09:56 +0100

    Update MainLayout.js
    organizing routes

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Router, Switch, Redirect } from "react-router-dom";
 
-import {history} from "../helpers/history"
-import PrivateRoute from "../components/PrivateRoute";
+import { history } from "../helpers/history";
+import Route from "../components/Route";
 
 import Author from "./Author/Author";
 import HomePage from "./HomePage";
@@ -11,26 +11,45 @@ import NotFoundPage from "./NotFoundPage";
 import Universities from "./Managing/Universities";
 import Schools from "./Managing/Schools";
 import Laboratories from "./Managing/Laboratories";
+import AccountSettings from "./Settings/AccountSettings";
+
+import LaboratoryHeads from "./Managing/LaboratoryHeads";
 //import Users from "./Managing/Users";
 
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import NavBar from "../components/layout/NavBar";
+import Teams from "./Managing/Teams";
+import { default as LHAssociation } from "./Managing/LaboratoryHeadsAssociation";
+import { default as TRAssociation } from "./Managing/TeamsResearchersAssociation";
 
 function MainLayout() {
-
   const routes = (
-    <Router history={history}>
-      <Switch>
-        <PrivateRoute path="/home" children={<HomePage />} />
-        {/* <PrivateRoute path="/users" children={<Users />} /> */}
-        <PrivateRoute path="/universities/" children={<Universities />} />
-        <PrivateRoute path="/schools/" children={<Schools />} />
-        <PrivateRoute path="/laboratories/" children={<Laboratories />} />
-        <PrivateRoute path="/author/:authorName" children={<Author />} /> }
-        <PrivateRoute path="/*" children={<NotFoundPage />} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/home" component={HomePage} />
+      <Route path="/universities" component={Universities} role="CED_HEAD" />
+      <Route path="/schools" component={Schools} role="CED_HEAD" />
+      <Route path="/laboratories" component={Laboratories} role="CED_HEAD" />
+      <Route
+        path="/laboratory-heads"
+        component={LaboratoryHeads}
+        role="CED_HEAD"
+      />
+      <Route
+        path="/laboratory-heads-association"
+        component={LHAssociation}
+        role="CED_HEAD"
+      />
+      <Route path="/teams" component={Teams} role="LABORATORY_HEAD" />
+      <Route
+        path="/teams-researchers-association"
+        component={TRAssociation}
+        role="LABORATORY_HEAD"
+      />
+      <Route path="/settings/account" component={AccountSettings} />
+      <Route path="/author/:authorName" component={Author} /> }
+      <Route path="/*" component={NotFoundPage} />
+    </Switch>
   );
 
   return (
@@ -39,7 +58,9 @@ function MainLayout() {
         <NavBar />
         <Header />
         <div className="my-3 my-md-5">
-          <div className="container">{routes}</div>
+          <div className="container">
+            <Router history={history}>{routes}</Router>
+          </div>
         </div>
       </div>
       <Footer />
