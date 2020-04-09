d87097a75578be436c932d9b934ba384a9181e6e
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-09 04:08:37 +0100

    Create SettingsAlert.js

---
+++ src/components/settings/SettingsAlert.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  HAS_NOT_CONFIRMED_MESSAGE,
+  INFORMATION_UPDATED_MESSAGE,
+  INFORMATION_NOT_UPDATED_MESSAGE,
+  PASSWORD_NOT_CONFIRMED_MESSAGE,
+  NO_MESSAGE,
+} from "./AlertMessages";
+
+function SettingsAlert(props) {
+  const alertMessage =
+    props.message === "HAS_NOT_CONFIRMED_MESSAGE"
+      ? HAS_NOT_CONFIRMED_MESSAGE
+      : props.message === "INFORMATION_UPDATED_MESSAGE"
+      ? INFORMATION_UPDATED_MESSAGE
+      : props.message === "INFORMATION_NOT_UPDATED_MESSAGE"
+      ? INFORMATION_NOT_UPDATED_MESSAGE
+      : props.message === "PASSWORD_NOT_CONFIRMED_MESSAGE"
+      ? PASSWORD_NOT_CONFIRMED_MESSAGE
+      : NO_MESSAGE;
+
+  return (
+    <div className="col-md-12">
+      <div
+        className={"alert alert-dismissible alert-" + props.badge ?? "success"}
+        role="alert"
+      >
+        <h3>{alertMessage.title}</h3>
+        <p>{alertMessage.message}</p>
+
+        <a href className="close" data-dismiss="alert" aria-label="close">
+          &times;
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default SettingsAlert;
