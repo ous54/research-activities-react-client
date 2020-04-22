04302a42b4d302a87f839be6a8d8b4226a292950
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-22 15:08:35 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,60 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./app/App";
-import { AuthProvider } from "./context/auth";
+import { Router, Switch, Route as PublicRoute } from "react-router-dom";
 
-render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
-  document.getElementById("root")
+import { createBrowserHistory } from "history";
+import { AppProvider } from "./AppContext";
+
+import Route from "./routes/Route";
+
+import LoginPage from "./views/Auth/LoginPage";
+import RegisterPage from "./views/Auth/RegisterPage";
+
+import MenuBar from "./views/_common/layout/MenuBar";
+import NavBar from "./views/_common/layout/NavBar";
+import Footer from "./views/_common/layout/Footer";
+
+import routes from "./routes/routes";
+
+import "./assets/css/datatables.css";
+import "./assets/css/tabler.css";
+
+import "popper.js/dist/popper";
+import "bootstrap/dist/js/bootstrap";
+
+const history = createBrowserHistory();
+
+const App = () => (
+  <AppProvider>
+    <Router history={history}>
+      <Switch>
+        <PublicRoute path="/login" component={LoginPage} />
+        <PublicRoute path="/register" component={RegisterPage} />
+        <Route path="/*" component={MainLayout} />
+      </Switch>
+    </Router>
+  </AppProvider>
 );
+
+const MainLayout = () => (
+  <div className="page">
+    <div className="flex-fill">
+      <NavBar />
+      <MenuBar />
+      <div className="my-3 my-md-5">
+        <div className="container">
+          <Router history={history}>
+            <Switch>
+              {routes.map((route, index) => (
+                <Route {...route} key={index} />
+              ))}
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
