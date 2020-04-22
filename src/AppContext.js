bdee81f7740ee9f488547cc3675d705f3b646930
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-04-22 15:09:32 +0000

    Create AppContext.js
    Globalize the app context

---
+++ src/AppContext.js
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer, useEffect } from "react";
+import makeApiServices from "./api/ApiServices";
+
+let reducer = (user, newUser) => {
+  if (newUser === null || newUser === undefined) {
+    localStorage.removeItem("user");
+    return initialState;
+  }
+  return newUser;
+};
+
+const initialState = null;
+
+const localState = JSON.parse(localStorage.getItem("user"));
+
+const AppContext = createContext();
+
+function AppProvider(props) {
+  const [user, setUser] = useReducer(reducer, localState || initialState);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  const ApiServices = makeApiServices(user ? user.token : null);
+
+  return (
+    <AppContext.Provider value={{ user, setUser, ApiServices }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+}
+
+export { AppContext, AppProvider };
