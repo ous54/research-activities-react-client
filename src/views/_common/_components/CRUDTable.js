1ae125d466bdf3cfcab334cbf777842da4e7f59d
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:09:16 +0000

    Create CRUD pages components

---
+++ src/views/_common/_components/CRUDTable.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import $ from "jquery";
+import "datatables";
+
+const CRUDTable = ({ columns, data, actions, tableSkeleton }) => {
+  useEffect(() => {
+    if (data.length) $(".datatable").DataTable();
+  }, [data]);
+
+  const objectToDisplayRow = (objectRow) =>
+    tableSkeleton.map(({ name }) => objectRow[name]);
+
+  return (
+    <div className="card">
+      <div className="table-responsive">
+        <table className="table card-table table-vcenter text-nowrap datatable">
+          <thead>
+            <tr>
+              {columns.map((column) => (
+                <th>{column}</th>
+              ))}
+              <th className="text-right">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((objectRow, index) => (
+              <tr key={index}>
+                {objectToDisplayRow(objectRow).map((element, index) => (
+                  <td
+                    style={{
+                      whiteSpace: element.length > 40 ? "break-spaces" : "nowrap ",
+                    }}
+                  >
+                    {element}
+                  </td>
+                ))}
+                <td className="text-right">
+                  <div className="btn-list d-block">
+                    {actions.map((action) => (
+                      <button
+                        type="button"
+                        onClick={() => {
+                          action.function(objectRow);
+                        }}
+                        className={`btn btn-sm btn-outline-${action.style}`}
+                      >
+                        {action.name}
+                      </button>
+                    ))}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default CRUDTable;
