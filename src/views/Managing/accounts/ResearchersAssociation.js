a3b7629b66ae64c798454156c33bcec08aae016c
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:25:21 +0000

    Update ResearchersAssociation.js
    Adding researchers association logic

--- src/views/Managing/accounts/ResearchersAssociation.js
+++ src/views/Managing/accounts/ResearchersAssociation.js
@@ -1,17 +1,140 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import PageHeader from "../../_common/_components/PageHeader";
 
-const ResearchersAssociation = (props) => {
+import { AppContext } from "../../../context/AppContext";
+import UserPicture from "../../_common/_components/UserPicture";
+import { Link } from "react-router-dom";
+const ResearchersAssociation = () => {
+  const { user, ApiServices, UserHelper } = useContext(AppContext);
+  const { userService, teamService } = ApiServices;
+
+  const [researchers, setResearchers] = useState([]);
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    updateData();
+  }, []);
+
+  const updateData = () => {
+    userService.getResearchers().then((response) => {
+      setResearchers(response.data);
+    });
+    teamService.findAllTeams().then((response) => {
+      setTeams(response.data);
+    });
+  };
+
+  const addToTeam = (team_id) => (user_id) => {
+    teamService.addUserToTeam(team_id, user_id).then((response) => {
+      updateData();
+    });
+  };
+
+  const removeFromTeam = (team_id) => (user_id) => {
+    teamService.removeFromTeam(team_id, user_id).then((response) => {
+      updateData();
+    });
+  };
+
   return (
     <div className="container">
-      <div className="page-header">
-        <div className="row align-items-center">
-          <div className="col-auto">
-            <h2 className="page-title"> Researchers Association</h2>
-          </div>
-        </div>
+      <PageHeader
+        title={`Équipes de votre laboratoire ${UserHelper.userHeadedLaboratories(
+          user
+        )}`}
+        subTitle={`${teams.length} équipe(s)`}
+      />
+      <div className="row">
+        {teams.map((team) => (
+          <TeamBox
+            addToTeam={addToTeam(team._id)}
+            removeFromTeam={removeFromTeam(team._id)}
+            team={team}
+            researchers={researchers}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
 export default ResearchersAssociation;
+
+const TeamBox = ({ researchers, team, addToTeam, removeFromTeam }) => {
+  const [selectedResearcher, setSelectedResearcher] = useState(null);
+
+  const handleSelectedResearcherChange = (event) => {
+    event.persist();
+    setSelectedResearcher(event.target.value);
+  };
+
+  const handSubmit = (event) => {
+    event.preventDefault();
+    addToTeam(selectedResearcher);
+  };
+  return (
+    <div className="col-md-6">
+      <div className="card">
+        <div className="card-header d-block">
+          <h3 className="card-title ">{team.abbreviation}</h3>
+          <div className=" text-muted text-4">{team.name}</div>
+        </div>
+        <div className="card-body">
+          <div className="row mb-n3">
+            {team.members.map((user) => (
+              <div className="col-6 row row-sm mb-3 align-items-center">
+                <UserPicture user={user} />
+                <div className="col text-truncate">
+                  <Link
+                    to={`/author/${user.firstName} ${user.lastName} `}
+                    className="text-body d-block text-truncate"
+                  >
+                    {`${user.firstName} ${user.lastName}`}
+                  </Link>
+
+                  <small className="d-block text-muted text-truncate mt-n1">
+                    <Link
+                      onClick={() => {
+                        removeFromTeam(user._id);
+                      }}
+                      className="text-body d-block text-truncate"
+                    >
+                      Retirer
+                    </Link>
+                  </small>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="card-body">
+          <form onSubmit={handSubmit}>
+            <label className="form-label m-2">Ajouter un chercher</label>
+
+            <select
+              required
+              name="selectedResearcher"
+              onChange={handleSelectedResearcherChange}
+              value={selectedResearcher}
+              className="form-control p-1"
+            >
+              <option selected disabled>
+                Sélectionner ici un chercher
+              </option>
+              {researchers.map(({ firstName, lastName, _id }, index) => (
+                <option value={_id} key={_id}>
+                  {`${firstName} ${lastName}`}
+                </option>
+              ))}
+            </select>
+            <div className="d-flex mt-2">
+              <button type="submit" className="btn btn-primary ml-auto">
+                Ajouter
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
