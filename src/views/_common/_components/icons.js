f6455f0a0db128d0ff8bcf5a863982e55c84533f
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:21:26 +0000

    Update icons.js
    Adding UserCheckIcon

--- src/views/_common/_components/icons.js
+++ src/views/_common/_components/icons.js
@@ -171,10 +171,10 @@ const ConfirmationIcon = () => (
   </svg>
 );
 
-const StatistiquesIcon = () => (
+const StatisticsIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-chart-area-line"
+    className="icon icon-tabler icon-tabler-chart-area-line"
     width="24"
     height="24"
     viewBox="0 0 24 24"
@@ -190,6 +190,26 @@ const StatistiquesIcon = () => (
   </svg>
 );
 
+const UserCheckIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-user-check"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" />
+    <circle cx="8.5" cy="7" r="4" />
+    <path d="M2 21v-2a4 4 0 0 1 4 -4h5a4 4 0 0 1 4 4v2" />
+    <path d="M16 11l2 2l4 -4" />
+  </svg>
+);
+
 export {
   HomeIcon,
   SettingsIcon,
@@ -200,5 +220,6 @@ export {
   NotificationIcon,
   TeamIcon,
   ConfirmationIcon,
-  StatistiquesIcon,
+  StatisticsIcon,
+  UserCheckIcon,
 };
