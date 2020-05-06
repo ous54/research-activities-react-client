fccc47170040d984b1cb18a87515665e27b89633
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:21:10 +0000

    Update UserPicture.js

--- src/views/_common/_components/UserPicture.js
+++ src/views/_common/_components/UserPicture.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const UserPicture = (props) => {
-  const { user, size } = props;
-
-  if (!size)
-    return (
-      <span className="avatar bg-blue-lt">
-        <span className="badge  bg-success"></span>
-        {user.firstName ? user.firstName[0] : ""}
-        {user.lastName ? user.lastName[0] : ""}
-      </span>
-    );
-
+const UserPicture = ({ user, size, badge }) => {
   return (
     <div className="col-auto">
-      <span className={`avatar  bg-blue-lt avatar-${size}`}>
-        {user.firstName ? user.firstName[0] : ""}
-        {user.lastName ? user.lastName[0] : ""}
-      </span>
+
+      {user.profilePicture && (
+        <span
+          className={`avatar avatar-${size}`}
+          style={{
+            backgroundImage: `url(${process.env.REACT_APP_BACKEND_API_URL}/pictures/${user.profilePicture})`,
+          }}
+        >
+          {badge && <span className="badge bg-green"></span>}
+        </span>
+      )}
+      {!user.profilePicture && (
+        <span className={`bg-blue-lt avatar avatar-${size}`}>
+          {user.firstName ? user.firstName[0] : ""}
+          {user.lastName ? user.lastName[0] : ""}
+        </span>
+      )}
+      
     </div>
   );
 };
