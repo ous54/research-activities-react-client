a39185d29a35e3a280b1e6b0b89497d122643bec
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:11:47 +0000

    Update routes.js
    Updating naming conventions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -15,72 +15,186 @@ import { default as RAssociation } from "../views/Managing/accounts/ResearchersA
 import FollowedResearchers from "../views/Managing/accounts/FollowedResearchers";
 import Statistics from "../views/Statistics/Statistics";
 
-const routes = [
-  {
-    path: "/home",
-    component: HomePage,
-  },
-  {
-    path: "/settings/account",
-    component: AccountSettings,
-  },
-  {
-    path: "/author/:authorName",
-    component: Author,
-  },
-  {
-    path: "/universities",
-    component: Universities,
-    role: ["CED_HEAD"],
-  },
-  {
-    path: "/schools",
-    component: Schools,
-    role: ["CED_HEAD"],
-  },
-  {
-    path: "/laboratories",
-    component: Laboratories,
-    role: ["CED_HEAD"],
-  },
-  {
-    path: "/teams",
-    component: Teams,
-    role: ["LABORATORY_HEAD"],
-  },
-  {
-    path: "/laboratory-heads",
-    component: LaboratoryHeads,
-    role: ["CED_HEAD"],
-  },
-  {
-    path: "/researchers",
-    component: Researchers,
-    role: ["LABORATORY_HEAD"],
-  },
-  {
-    path: "/laboratory-heads-association",
-    component: LHAssociation,
-    role: ["CED_HEAD"],
-  },
-  {
-    path: "/followed-researchers",
-    component: FollowedResearchers,
-    role: ["CED_HEAD"],
-  },
-  {
-    path: "/researchers-association",
-    component: RAssociation,
-    role: ["LABORATORY_HEAD"],
-  },
-  {
-    path: "/statistics",
-    component: Statistics,
-  },
-  {
-    path: "/*",
-    component: NotFoundPage,
-  },
+import {
+  HomeIcon,
+  ConfigurationIcon,
+  StatisticsIcon,
+  UserCheckIcon,
+  TeamIcon,
+  SettingsIcon,
+} from "../views/_common/_components/icons";
+
+const allRoles = ["ADMIN", "CED_HEAD", "LABORATORY_HEAD", "RESEARCHER"];
+
+const entitiesPathsCategory = {
+  title: "Entités",
+  isDropdown: true,
+  icon: SettingsIcon,
+  routes: [
+    {
+      title: "Universités",
+      path: "/universities",
+      component: Universities,
+      roles: ["ADMIN", "CED_HEAD"],
+      inMenu: true,
+    },
+    {
+      title: "Écoles",
+      path: "/schools",
+      component: Schools,
+      roles: ["ADMIN", "CED_HEAD"],
+      inMenu: true,
+    },
+    {
+      title: "Laboratoires",
+      path: "/laboratories",
+      component: Laboratories,
+      roles: ["ADMIN", "CED_HEAD"],
+      inMenu: true,
+    },
+    {
+      title: "Équipes",
+      path: "/teams",
+      component: Teams,
+      roles: ["ADMIN", "LABORATORY_HEAD"],
+      inMenu: true,
+    },
+  ],
+};
+
+const accountsManagementPathsCategory = {
+  title: "Gestion des comptes",
+  isDropdown: true,
+  icon: TeamIcon,
+  routes: [
+    {
+      title: "Chefs de Laboratoires",
+      path: "/laboratory-heads",
+      component: LaboratoryHeads,
+      roles: ["ADMIN", "CED_HEAD"],
+      inMenu: true,
+    },
+    {
+      title: "Chercheurs",
+      path: "/researchers",
+      component: Researchers,
+      roles: ["ADMIN", "LABORATORY_HEAD"],
+      inMenu: true,
+    },
+    {
+      title: "Association entre Chefs et Laboratoires",
+      path: "/laboratory-heads-association",
+      component: LHAssociation,
+      roles: ["ADMIN", "CED_HEAD"],
+      inMenu: true,
+    },
+    {
+      title: "researchers-association",
+      path: "/researchers-association",
+      component: RAssociation,
+      roles: ["ADMIN", "LABORATORY_HEAD"],
+      inMenu: true,
+    },
+  ],
+};
+
+const StatisticsPaths = {
+  title: "Statistiques",
+  isDropdown: false,
+  icon: StatisticsIcon,
+  routes: [
+    {
+      title: "Statistiques",
+      path: "/statistics",
+      component: Statistics,
+      icon: StatisticsIcon,
+      inMenu: true,
+      roles: allRoles,
+    },
+  ],
+};
+
+const followedResearchersPaths = {
+  title: "Chercheur suivis",
+  isDropdown: false,
+  routes: [
+    {
+      title: "Chercheur suivis",
+      path: "/followed-researchers",
+      component: FollowedResearchers,
+      icon: UserCheckIcon,
+      roles: ["ADMIN", "CED_HEAD"],
+      inMenu: true,
+    },
+  ],
+};
+const communPathsCategory = {
+  isDropdown: false,
+  routes: [
+    {
+      title: "Accueil",
+      path: "/home",
+      component: HomePage,
+      icon: HomeIcon,
+      inMenu: true,
+      roles: allRoles,
+    },
+    {
+      title: "Compte",
+      path: "/account",
+      component: AccountSettings,
+      inMenu: false,
+      roles: allRoles,
+    },
+  ],
+};
+
+const authorPathsCategory = {
+  isDropdown: false,
+  inMenu: false,
+  routes: [
+    {
+      title: "Auteur",
+      path: "/author/:authorName",
+      component: Author,
+      inMenu: false,
+      roles: allRoles,
+    },
+  ],
+};
+
+const errorPathsCategory = {
+  title: "Errors",
+  isDropdown: false,
+  routes: [
+    {
+      title: "Errors",
+      path: "/*",
+      component: NotFoundPage,
+      inMenu: false,
+      roles: allRoles,
+    },
+  ],
+};
+
+const menus = [
+  communPathsCategory,
+  authorPathsCategory,
+  entitiesPathsCategory,
+  accountsManagementPathsCategory,
+  followedResearchersPaths,
+  StatisticsPaths,
+  errorPathsCategory,
 ];
 
-export default routes;
+const routes = [
+  ...communPathsCategory.routes,
+  ...authorPathsCategory.routes,
+  ...entitiesPathsCategory.routes,
+  ...accountsManagementPathsCategory.routes,
+  ...followedResearchersPaths.routes,
+  ...StatisticsPaths.routes,
+  ...errorPathsCategory.routes,
+];
+
+export { routes, menus };
