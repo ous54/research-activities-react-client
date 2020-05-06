ffc842d33c4416244c9291bc24ebcbbcee74031b
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-06 13:11:40 +0000

    Update services.js
    Updating naming conventions

--- src/api/services.js
+++ src/api/services.js
@@ -8,57 +8,61 @@ const makeAuthorService = (api) => ({
 });
 
 const makeUserService = (api) => ({
-  createUser: (user) => api.post(`/user`, user),
-  updateUser: (user) => api.put(`/user`, user),
-  findUser: (_id) => api.get(`/user/${_id}`),
-  findAllUsers: () => api.get(`/user`),
-  deleteUser: (_id) => api.delete(`/user/${_id}`),
+  createUser: (user) => api.post(`/users`, user),
+  updateUser: (user) => api.put(`/users`, user),
+  findUser: (_id) => api.get(`/users/${_id}`),
+  findAllUsers: () => api.get(`/users`),
+  deleteUser: (_id) => api.delete(`/users/${_id}`),
   updatePassword: (_id, passwords) =>
-    api.post(`/user/${_id}/update-password`, passwords),
-  getLabHeads: () => api.get(`/lab-heads`),
+    api.post(`/users/${_id}/update-password`, passwords),
+  updateProfilePicture: (formData) =>
+    api.post(`/upload-profile-picture`, formData),
+  getLaboratoryHeads: () => api.get(`/laboratory-heads`),
+  getResearchers: () => api.get(`/researchers`),
   followUser: (user) => api.post(`/follow`, user),
   unfollowUser: (_id) => api.get(`/unfollow/${_id}`),
   isFollowing: (name) => api.get(`/is-following/${name}`),
-  getFollowedUsers: () => api.get(`/followed-users`),
+  getFollowedUsers: (filter) => api.get(`/followed-users`, { params: filter }),
+  getFilteringOptions: () => api.get(`/filtering-options`),
 });
 
 const makeUniversityService = (api) => ({
-  createUniversity: () => api.post(`/university`),
-  updateUniversity: () => api.put(`/university`),
-  findUniversity: (_id) => api.get(`/university/${_id}`),
-  findAllUniversities: () => api.get(`/university`),
-  deleteUniversity: (_id) => api.delete(`/university/${_id}`),
-  getUniversitySchools: (_id) => api.get(`/university/${_id}/schools`),
+  createUniversity: (university) => api.post(`/universities`, university),
+  updateUniversity: (university) => api.put(`/universities`, university),
+  findUniversity: (_id) => api.get(`/universities/${_id}`),
+  findAllUniversities: () => api.get(`/universities`),
+  deleteUniversity: (_id) => api.delete(`/universities/${_id}`),
+  getUniversitySchools: (_id) => api.get(`/universities/${_id}/schools`),
 });
 
 const makeSchoolService = (api) => ({
-  createSchool: (school) => api.post(`/school`, school),
-  updateSchool: () => api.put(`/school`),
-  findSchool: (_id) => api.get(`/school/${_id}`),
-  findAllSchools: () => api.get(`/school`),
-  deleteSchool: (_id) => api.delete(`/school/${_id}`),
-  getSchoolLaboratories: (_id) => api.get(`/school/${_id}/laboratories`),
+  createSchool: (school) => api.post(`/schools`, school),
+  updateSchool: (school) => api.put(`/schools`, school),
+  findSchool: (_id) => api.get(`/schools/${_id}`),
+  findAllSchools: () => api.get(`/schools`),
+  deleteSchool: (_id) => api.delete(`/schools/${_id}`),
+  getSchoolLaboratories: (_id) => api.get(`/schools/${_id}/laboratories`),
 });
 
 const makeLaboratoryService = (api) => ({
-  createLaboratory: (laboratory) => api.post(`/laboratory`, laboratory),
-  updateLaboratory: (laboratory) => api.put(`/laboratory`, laboratory),
-  findLaboratory: (_id) => api.get(`/laboratory/${_id}`),
-  findAllLaboratories: () => api.get(`/laboratory`),
-  deleteLaboratory: (_id) => api.delete(`/laboratory/${_id}`),
-  getTeamsOfLaboratory: (_id) => api.get(`/laboratory/${_id}/teams`),
-  getLaboratoryOfHead: (head_id) => api.get(`/laboratory-of-head/${head_id}`),
+  createLaboratory: (laboratory) => api.post(`/laboratories`, laboratory),
+  updateLaboratory: (laboratory) => api.put(`/laboratories`, laboratory),
+  findLaboratory: (_id) => api.get(`/laboratories/${_id}`),
+  findAllLaboratories: () => api.get(`/laboratories`),
+  deleteLaboratory: (_id) => api.delete(`/laboratories/${_id}`),
+  getTeamsOfLaboratory: (_id) => api.get(`/laboratories/${_id}/teamss`),
+  getLaboratoryOfHead: (head_id) => api.get(`/laboratories-of-head/${head_id}`),
   getFreeLaboratories: () => api.get(`/free-laboratories`),
   associateHeadToLaboratory: (head_id, lab_id) =>
     api.get(`/entitle-laboratory/${head_id}/${lab_id}`),
 });
 
 const makeTeamService = (api) => ({
-  createTeam: (team) => api.post(`/team`, team),
-  updateTeam: (team) => api.put(`/team`, team),
-  findAllTeams: () => api.get(`/team`),
-  findTeam: (_id) => api.get(`/team/${_id}`),
-  deleteTeam: (_id) => api.delete(`/team/${_id}`),
+  createTeam: (team) => api.post(`/teams`, team),
+  updateTeam: (team) => api.put(`/teams`, team),
+  findAllTeams: () => api.get(`/teams`),
+  findTeam: (_id) => api.get(`/teams/${_id}`),
+  deleteTeam: (_id) => api.delete(`/teams/${_id}`),
   addUserToTeam: (team_id, user_id) =>
     api.get(`/add-to-team/${team_id}/${user_id}`),
   removeFromTeam: (team_id, user_id) =>
@@ -80,13 +84,3 @@ export {
   makeAuthentificationService,
   makeStatisticsService,
 };
-
-/*
-
-
-
-
-})
-
-
-*/
