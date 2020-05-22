18da44f51902cae0f7fc84b40bd36f209a5dd2f4
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-05-22 13:12:19 +0000

    Create AuthorSearch.js

---
+++ src/views/Author/AuthorSearch.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useContext, useState } from "react";
+
+import { useParams, Link } from "react-router-dom";
+
+import { AppContext } from "../../context/AppContext";
+import PageHeader from "../_common/_components/PageHeader";
+import AuthorCard from "./_components/AuthorCard";
+
+import image from "../../assets/images/illustrations/undraw_quitting_time_dm8t.svg";
+
+import { LoopIcon } from "../_common/_components/icons";
+
+const AuthorSearch = () => {
+  let { authorName } = useParams();
+
+  const [authors, setAuthors] = useState([]);
+  const [noResult, setNoResult] = useState(false);
+
+  const { user, ApiServices } = useContext(AppContext);
+  const { scraperService, userService } = ApiServices;
+  useEffect(() => {
+    if (noResult) setNoResult(false);
+
+    const authorNamePath = authorName.replace(" ", "%20");
+
+    setAuthors([]);
+
+    scraperService.authorSearch(authorNamePath).then((result) => {
+      if (result.data.error) setNoResult(true);
+      else setAuthors(result.data);
+    });
+  }, [authorName]);
+
+  return (
+    <div className="container">
+      <PageHeader
+        title={"Chercher l'auteur : " + authorName}
+        subTitle={authors.length ? authors.length + " chercheurs" : ""}
+      />
+      <div className="row">
+        {authors.map((author) => (
+          <AuthorCard author={author} />
+        ))}
+
+        {authors.length === 0 && !noResult && (
+          <div className="mt-4 loader container "></div>
+        )}
+      </div>
+
+      {noResult && (
+        <div className="empty">
+          <div className="empty-icon">
+            <img src={image} className="h-8 mb-4" alt="" />
+          </div>
+          <p className="empty-title h3">
+            Aucun résultat trouvé pour {authorName}
+          </p>
+          <p className="empty-subtitle text-muted">
+            Essayez d'ajuster votre recherche ou votre filtre pour trouver
+            l'auteur que vous recherchez.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AuthorSearch;
