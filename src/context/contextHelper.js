4494498291584a7a8cbd2018e531614fcad8c4cd
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-06-12 00:20:10 +0100

    Update contextHelper.js
    Adding teamsHeaded to short bio

--- src/context/contextHelper.js
+++ src/context/contextHelper.js
@@ -1,22 +1,29 @@
-const userShortBio = ({ laboratoriesHeaded, teamsMemberships, ...user }) => {
+const userShortBio = ({
+  laboratoriesHeaded,
+  teamsHeaded,
+  teamsMemberships,
+  ...user
+}) => {
   if (user.role === "CED_HEAD") return `chef de CED`;
 
   const laboratoriesHeadedString = laboratoriesHeaded
     .map(({ abbreviation }) => `chef de laboratoire ${abbreviation}`)
     .join(" , ");
 
+  const teamsHeadedString = teamsHeaded
+    .map(({ abbreviation }) => `chef de l'équipe ${abbreviation}`)
+    .join(" , ");
+
   const teamsMembershipsString = teamsMemberships
     .map(({ abbreviation }) => `membre de l'équipe ${abbreviation}`)
     .join(" , ");
 
-  if (laboratoriesHeadedString.length && teamsMembershipsString)
-    return `${laboratoriesHeadedString} et ${teamsMembershipsString}`;
-
-  if (laboratoriesHeadedString.length) return `${laboratoriesHeadedString}`;
-
-  if (teamsMembershipsString.length) return `${teamsMembershipsString}`;
-
-  return `Chercheur`;
+  const shortBio =
+    laboratoriesHeadedString +
+    " " +
+    (teamsHeadedString ?? teamsMembershipsString);
+  if (shortBio.length > 10) return shortBio;
+  else return "Chercheur";
 };
 
 const userHeadedLaboratories = ({ laboratoriesHeaded }) =>
