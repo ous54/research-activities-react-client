2b0f963e011b63a8a02911093349c3b0a7d768bd
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-06-15 10:16:49 +0100

    Update Coauthors.js

--- src/views/Author/_components/Coauthors.js
+++ src/views/Author/_components/Coauthors.js
@@ -10,33 +10,34 @@ const Coauthors = withRouter(({ history, ...props }) => {
       </div>
       <div className="card-body o-auto" style={{ height: "auto" }}>
         <ul className="list-unstyled list-separated">
-          {props.author.coauthors.map((coauthor) => (
-            <li className="list-separated-item" key={coauthor.name}>
-              <div className="row align-items-center">
-                <div className="col-auto">
-                  <span
-                    className="avatar avatar-md d-block"
-                    style={{
-                      backgroundImage: "url(" + coauthor.profilePicture + ")",
-                    }}
-                  ></span>
-                </div>
-                <div className="col">
-                  <div>
-                    <Link
-                      to={"/author-search/" + coauthor.name}
-                      className="text-inherit"
-                    >
-                      {coauthor.name}
-                    </Link>
+          {props.author.coauthors &&
+            props.author.coauthors.map((coauthor) => (
+              <li className="list-separated-item" key={coauthor.name}>
+                <div className="row align-items-center">
+                  <div className="col-auto">
+                    <span
+                      className="avatar avatar-md d-block"
+                      style={{
+                        backgroundImage: "url(" + coauthor.profilePicture + ")",
+                      }}
+                    ></span>
                   </div>
-                  <small className="d-block item-except text-sm text-muted h-1x">
-                    {coauthor.bio}
-                  </small>
+                  <div className="col">
+                    <div>
+                      <Link
+                        to={"/author-search/" + coauthor.name}
+                        className="text-inherit"
+                      >
+                        {coauthor.name}
+                      </Link>
+                    </div>
+                    <small className="d-block item-except text-sm text-muted h-1x">
+                      {coauthor.bio}
+                    </small>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            ))}
         </ul>
       </div>
     </div>
