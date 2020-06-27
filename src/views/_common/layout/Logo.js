d47ac713a9e0e13c5e98c24ef2c513ec74b191f6
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-06-27 20:06:08 +0100

    Update Logo.js
    Updating app icon

--- src/views/_common/layout/Logo.js
+++ src/views/_common/layout/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import image from "../../../assets/images/tabler.svg";
+import image from "../../../assets/images/tabler.png";
 
 const Logo = () => (
   <Link
