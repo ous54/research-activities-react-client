282cb3a40b48b2ad752f2471831c82521ae4e482
Akram Aznakour <35656299+akramaznakour@users.noreply.github.com>
2020-08-09 23:34:40 +0100

    Fix author card size (#69)

--- src/views/Author/components/AuthorCard.js
+++ src/views/Author/components/AuthorCard.js
@@ -18,7 +18,7 @@ const ScopusCard = ({ author }) => (
   <div className="row row-sm align-items-center">
     <div className="col-auto">
       <span className="avatar  avatar-md ">
-        {author.name.split(" ")[0][0] + " " + author.name.split(" ")[1][0]}
+        {author.name.split(" ")[0][0] + author.name.split(" ")[1][0]}
       </span>
     </div>
     <div className="col" style={{ minWidth: "auto" }}>
@@ -26,7 +26,7 @@ const ScopusCard = ({ author }) => (
         className="text-body d-block pb-2"
         to={"/author/" + author.authorId}
       >
-        {author.name ? author.name : ""}
+        {author.name ? author.name.substr(0,25) : ""}
         <span className={"badge pull-right ml-2 bg-orange"}>
           {author.platform}
         </span>
@@ -63,22 +63,24 @@ const ScholarCard = ({ author }) => (
         className="text-body d-block pb-2"
         to={"/author/" + author.authorId}
       >
-        {author.name ? author.name : ""}
+        {author.name ? author.name.substr(0,25) : ""}
         <span className="badge pull-right ml-2 bg-blue">{author.platform}</span>
       </Link>
 
       <small className="d-block text-muted text-truncate mt-n1">
-        {author.interests.slice(0, 2).map((interest, index) => (
-          <span key={index} className="badge bg-blue-lt  mr-2">
-            {interest.length > 30 / (1 + index)
-              ? interest.substr(1, 30 / (1 + index)).concat("...")
-              : interest}
-          </span>
-        ))}
-        {author.interests.length > 2 && <span>...</span>}
+        {author.interests
+          .join("@")
+          .substr(0,35)
+          .trim()
+          .split("@")
+          .filter((interest) => interest.length > 2)
+          .map((interest) => (
+            <span className="badge bg-blue-lt  mr-2">{interest}</span>
+          ))}
       </small>
     </div>
   </div>
 );
+
 
 export default AuthorCard;
